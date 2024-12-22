7267f8d8a7cce14e901b692f0615f331ae73ba2b
arizaxel16 <arizaxel16@gmail.com>
2024-12-22 11:57:13 -0500

    Add extra flags

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -60,12 +60,18 @@ const Splash = () => {
 				selected={flagSelected}
 				onSelect={(code) => {
 					setFlagSelected(code);
-					newLanguage("US" === code ? "en" : "es");
+					newLanguage(code === "US" || code === "GB" ? "en" : "es");
 				}}
-				countries={["US", "ES"]}
+				countries={["US", "GB", "ES", "CO"]}
 				className="flag-selector"
 				selectButtonClassName="flag-pop-up-menu"
-				customLabels={{ US: translations.usFlag, ES: translations.esFlag }}
+				customLabels={{
+					US: translations.usFlag,
+					GB: translations.ukFlag,
+					ES: translations.esFlag,
+					CO: translations.colFlag,
+				}}
+				placeholder="Select a country"
 			/>
 
 			{!isVideoLoaded && <div className="intro-bg-fallback"></div>}
--- src/pages/Splash/SplashStrings.js
+++ src/pages/Splash/SplashStrings.js
@@ -2,16 +2,20 @@ const en = {
 	name: "AXEL ARIZA",
 	profileDescription: "Software Engineer | FullStack Developer",
     btn: "Enter",
-	usFlag: "English",
-	esFlag: "Spanish"
+	usFlag: "United States (English)",
+	esFlag: "Spain (Spanish)",
+	ukFlag: "United Kingdom (English)",
+	colFlag: "Colombia (Spanish)"
 };
 
 const es = {
 	name: "AXEL ARIZA",
 	profileDescription: "Ingeniero de Software | Desarrollador FullStack",
     btn: "Entrar",
-	usFlag: "Inglés",
-	esFlag: "Español"
+	usFlag: "Estados Unidos (Inglés)",
+	esFlag: "España (Español)",
+	ukFlag: "Reino Unido (Inglés)",
+	colFlag: "Colombia (Español)"
 };
 
 export { en, es };
