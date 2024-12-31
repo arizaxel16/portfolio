fc489c0210c7219287f783fc8e7fae06361b514d
arizaxel16 <arizaxel16@gmail.com>
2024-12-31 11:03:48 -0500

    Add navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Home.scss";
+import NavBar from "../../components/NavBar/NavBar";
 
 function Home() {
 	return (
 		<div className="container-home">
-			HELLO
+			<NavBar />
 		</div>
 	);
 }
