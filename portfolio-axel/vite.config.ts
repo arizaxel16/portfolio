8fa10097f112c40209010783e6306c07aac56cd1
arizaxel16 <arizaxel16@gmail.com>
2025-05-29 10:43:42 -0500

    deployment script

--- portfolio-axel/vite.config.ts
+++ portfolio-axel/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio/', // <--- Add this line!
+  base: '/portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
